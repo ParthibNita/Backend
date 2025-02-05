0726e2a70f156a025c76929f0bd40a72f6ec1c4d
Parthib Deb <debparthib0040@gmail.com>
2025-02-05 18:57:20 +0530

    debug login functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -117,7 +117,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
 
   // 2
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
