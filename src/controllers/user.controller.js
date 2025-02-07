f30bb5223bb9c21eb4e4741bfa54bcff1aca3074
Parthib Deb <debparthib0040@gmail.com>
2025-02-07 12:10:12 +0530

    Add updateAccountDetails functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -286,6 +286,32 @@ const getCurrrentUser = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
 });
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!(fullName || email)) {
+    throw new ApiError(400, "fullName or email is required");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  // if (!user) {
+  //   throw new ApiError(500, "error while updating user details");
+  // }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
 export {
   registerUser,
   loginUser,
@@ -293,4 +319,5 @@ export {
   refreshAccessToken,
   changePassword,
   getCurrrentUser,
+  updateAccountDetails,
 };
