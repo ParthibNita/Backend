e433732e22c57595610bd1d405b6a004f8474809
Parthib Deb <debparthib0040@gmail.com>
2025-06-13 19:19:11 +0530

    Add secured routes for user account management and profile updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logOutUser,
   registerUser,
   refreshAccessToken,
+  changePassword,
+  getCurrrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUsercoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
@@ -30,5 +37,15 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/currentUser").get(verifyJWT, getCurrrentUser);
+router.route("/updateAccount").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatarUpdate")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImageUpdate")
+  .patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/History").get(verifyJWT, getWatchHistory);
 export default router;
